b3e9efc910695af2f56ae86da9fc8553db092e2e
Boeun <boeun0904@naver.com>
2023-06-27 00:00:12 +0900

    Feat: implement add postit paper

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const onClickButton = () => {
-  const hello = document.querySelector(".hello");
-  hello.innerHTML = "안녕띠~";
-  hello.classList.add("background-aliceblue");
+const addPaper = () => {
+  const paper = document.createElement("div");
+  paper.classList.add("postit-paper");
+
+  const paper_header = document.createElement("div");
+  paper_header.classList.add("postit-paper-header");
+
+  const button = document.createElement("button");
+  button.classList.add("delete-button");
+
+  const paper_body = document.createElement("div");
+  paper_body.classList.add("postit-paper-body");
+
+  const paper_content = document.createElement("textarea");
+  paper_content.classList.add("postit-paper-content");
+
+  paper_header.appendChild(button);
+  paper_body.appendChild(paper_content);
+  paper.appendChild(paper_header);
+  paper.appendChild(paper_body);
+
+  document.querySelector(".paper-container").appendChild(paper);
 };
 
-const button = document.querySelector("#submit-button");
-button.addEventListener("click", onClickButton);
+const addButton = document.querySelector(".add-button");
+addButton.addEventListener("click", addPaper);
